1a40578990f50c091c159517af499cef869766a6
S.Khalezov <energet666@gmail.com>
2026-05-01 22:06:50 +0500

    edit vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,27 +1,28 @@
-import tailwindcss from '@tailwindcss/vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const apiTarget = env.VITE_API_TARGET || 'http://localhost:8080';
+	const env = loadEnv(mode, process.cwd(), "");
+	const apiTarget = env.VITE_API_TARGET || "http://localhost:8080";
 
-  return {
-    plugins: [svelte(), tailwindcss()],
-    server: {
-      host: '0.0.0.0',
-      port: 5173,
-      strictPort: true,
-      proxy: {
-        '/api': {
-          target: apiTarget,
-          changeOrigin: true
-        },
-        '/media': {
-          target: apiTarget,
-          changeOrigin: true
-        }
-      }
-    }
-  };
+	return {
+		plugins: [svelte(), tailwindcss()],
+		server: {
+			host: "0.0.0.0",
+			port: 5173,
+			strictPort: true,
+			proxy: {
+				"/api": {
+					target: apiTarget,
+					changeOrigin: true,
+				},
+				"/media": {
+					target: apiTarget,
+					changeOrigin: true,
+				},
+			},
+			allowedHosts: "all",
+		},
+	};
 });
